dbd529cc5278ba23feb71f5b1711940b79c1bfd4
feherszabolcs <feherszabi2005@gmail.com>
2022-03-09 20:04:49 +0100

    Add project files.

---
+++ Megoldas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace MoonExp2022
+{
+    class Megoldas
+    {
+        private List<Reteg> Retegek = new List<Reteg>();
+
+        public int LegmelyebbAlsoLeghatar { get; private set; }
+        public int LegmelyebbAlsoLeghatarSorszama { get; private set; }
+        public int MeresekSzama => Retegek[0].MeresekSzama;
+        public int RetegekSzama => Retegek.Count;
+        
+        public int TeljesenElvekonyodottRetegekSzama => Retegek.Where(r => r.ElvekonyodottTeljesen).Count();
+
+
+        public int MinVastagsag(int retegSorszam)
+        {
+            Reteg r = Retegek[retegSorszam];
+            return r.MinVastagsag;
+        }
+        public int MaxVastagsag(int retegSorszam)
+        {
+            Reteg r = Retegek[retegSorszam];
+            return r.MaxVastagsag;
+        }
+
+        public Megoldas(string forras)
+        {
+            foreach (var item in File.ReadAllLines(forras))
+            {
+                Reteg r = new Reteg(item);
+                Retegek.Add(r);
+            }
+        }
+    }
+}
---
+++ Reteg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MoonExp2022
+{
+    class Reteg
+    {
+        //public List<Lencse> MyProperty;
+        private Dictionary<int, int> Meresek = new Dictionary<int, int>();
+
+        public bool ElvekonyodottTeljesen => Meresek.Values.Contains(0) ? true : false;
+        public int LehetsegesLencsekSzama { get; private set; }
+        public int MaxVastagsag => Meresek.Values.Max();
+        public int MeresekSzama => Meresek.Count;
+        public int MinVastagsag => Meresek.Values.Min();
+        public int TenylegesLencsekSzama { get; private set; }
+
+        private void LehetsegesLencsekKeresese()
+        {
+
+        }
+
+        public int RetegVastagsaga(int meresSorszama)
+        {
+            return 0;
+        }
+
+        public Reteg(string meresek)
+        {
+            string[] m = meresek.Split(' ');
+            foreach (var item in m)
+            {
+                string[] s = item.Split('-');
+                Meresek.Add(int.Parse(s[0]), int.Parse(s[1]));
+            }
+        }
+    }
+}
